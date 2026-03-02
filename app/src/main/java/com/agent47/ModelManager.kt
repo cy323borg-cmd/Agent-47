7f788e3aa59175c4649f14e275c46bd0d086ee25
cy323borg-cmd <cy323borg@gmail.com>
2026-03-02 22:45:25 +0600

    Create ModelManager.kt

---
+++ app/src/main/java/com/agent47/ModelManager.kt
@@ -0,0 +1,12 @@
+package com.agent47
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+
+object ModelManager {
+
+    fun loadModel(context: Context, uri: Uri) {
+        Log.d("Agent47", "Model Selected: $uri")
+    }
+}
